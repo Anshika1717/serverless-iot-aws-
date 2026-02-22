786bb4e452f1c56aef6a836cc16b47140500fc62
ANSHIKA KUMARI <anshika17104@gmail.com>
2026-02-22 09:45:24 +0530

    Implement Lambda function to store IoT data in DynamoDB

--- lambda-iot-to-dynamodb/lambda_function.py
+++ lambda-iot-to-dynamodb/lambda_function.py
@@ -1,1 +1,30 @@
+import json
+import boto3
+import time
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('iot-data-table')
+
+def lambda_handler(event, context):
+    
+    temperature = event['temperature']
+    humidity = event['humidity']
+    light = event['light']
+    air_quality = event['air_quality']
+    
+    timestamp = int(time.time())
+    
+    table.put_item(
+        Item={
+            'timestamp': timestamp,
+            'temperature': temperature,
+            'humidity': humidity,
+            'light': light,
+            'air_quality': air_quality
+        }
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Data stored successfully')
+    }
